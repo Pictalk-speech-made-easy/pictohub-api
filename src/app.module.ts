2ac806f2080e1afab0991cb127a3e6717083e880
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2024-05-13 13:29:42 +0200

    added sentry to the API

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { PictohubModule } from './pictohub/pictohub.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { PictalkModule } from './pictalk/pictalk.module';
+import { SentryModule } from '@ntegral/nestjs-sentry';
 
 @Module({
   imports: [
@@ -17,7 +18,11 @@ import { PictalkModule } from './pictalk/pictalk.module';
       max: 10000,
       isGlobal: true,
     }),
-
+    SentryModule.forRoot({
+      dsn: 'https://f6a20c9301bdf98c3caa9b33c415bf3f@o1135783.ingest.us.sentry.io/4507248851812352',
+      environment: process.env.NODE_ENV,
+      logLevels: ['debug']
+    }),
     PictohubModule,
     PictalkModule,
   ],
